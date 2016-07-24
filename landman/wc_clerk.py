26dd7ea9c07a036442c6c11ca0c20b215ed20835
estenssoros <seb.estenssoro@gmail.com>
2016-07-24 12:23:26 -0600

    updated

--- landman/wc_clerk.py
+++ landman/wc_clerk.py
@@ -2,14 +2,7 @@ from LM_Text import loop_text
 from LM_OCR import loop_ocr
 from LM_AWS import sync_read, write_to_s3, connect_s3
 from os import system
-
-def check_docs():
-    needs_ocr = []
-    b = connect_s3
-    for fname in b.list('textdocs/'):
-        if fname.endswith('.txt'):
-            key = b.new_key(fname)
-            # text = 
+import pandas as pd
 
 if __name__ == '__main__':
     system('clear')
@@ -19,3 +12,11 @@ if __name__ == '__main__':
     print '  -loop_ocr(#loops)'
     print '  -sync_read(r=True)'
     print '  -write_to_s3(file)'
+    df = pd.DataFrame(columns = ['doc','w_count'])
+    b = connect_s3
+    for key in b.list('textdocs/'):
+        if key.name.endswith('.txt'):
+            text = key.get_contents_as_string()
+            text = text.replace('\n',' ')
+            doc = key.name.replace('txtdocs/','').replace('.txt','')
+            df = df.append({'doc':doc,'w_count':len(text.split()},ignore_index=True)

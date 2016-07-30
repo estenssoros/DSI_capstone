530c8de738636d8cbde17a3c64f9eeee60e3976f
estenssoros <seb.estenssoro@gmail.com>
2016-07-30 17:02:22 -0600

    implementing segmentation

--- landman/wc_clerk.py
+++ landman/wc_clerk.py
@@ -18,6 +18,7 @@ from string import maketrans, punctuation
 import os
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_text_df(fname):
@@ -28,14 +29,20 @@ def get_text_df(fname):
     return df
 
 
-def make_trie(text, window=7):
+def text_windows(text, window):
     start = 0
     end = start + window
     tries = []
-    while len(text) > 0:
+    while end <= len(text):
         tries.append(text[start:end])
-        text = text[1:]
+        start += 1
+        end += 1
+    return tries
 
+
+def make_trie(text, window=7):
+    # tries = text_windows(text, window)
+    tries = text.split()
     _end = '_end_'
     root = dict()
     for trie in tries:
@@ -63,13 +70,59 @@ def read_text(fname):
 def score_word(word, trie):
     if len(word) == 0:
         return 0
-    print word
     letter = word[0]
     if letter in trie:
-        return  1 + score_word(word[1:], trie[letter])
+        return 1 + score_word(word[1:], trie[letter])
     else:
         return 0
 
+
+def lsv(word, trie):
+    if len(word) == 0:
+        return len(trie)
+
+    letter = word[0]
+    if letter in trie:
+        return lsv(word[1:], trie[letter])
+    else:
+        return len(trie)
+
+
+def weight_node(trie):
+    weight = 0
+    for k, v in trie.iteritems():
+        if k == '_end_':
+            return 1
+        else:
+            weight += weight_node(trie[k])
+    return weight
+
+
+def entropy(trie):
+    n_weight = weight_node(trie)
+    return sum([weight_node(n) / n_weight * np.log(weight_node(n)) / n_weight for c, n in trie.iteritems()])
+
+
+def in_trie(segment, trie):
+    if len(segment) == 0:
+        return True
+    letter = segment[0]
+    if letter in trie:
+        return in_trie(segment[1:], trie[letter])
+    else:
+        return False
+
+
+def in_windows(windows, trie):
+    return [w for w in windows if in_trie(w, trie)]
+
+
+def segment_text(text, f_trie, b_trie):
+    size = 6
+    f_windows = text_windows(text, size)
+    b_windows = text_windows(text[::-1], size)
+    
+    return f_windows, b_windows
 
 if __name__ == '__main__':
     system('clear')
@@ -78,20 +131,24 @@ if __name__ == '__main__':
     # text = df.loc[0, 'text']
     # df['char_tries'] = df.apply(lambda x: character_trie(7, x['text']), axis=1)
     # df['b_char_tries'] = df.apply(lambda x: character_trie(7, x['text'][::-1]), axis=1)
-    # docs = [f for f in os.listdir('textdocs/') if f.endswith('.txt')]
-    # for doc in docs:
-    #     correct = read_text('train_text/{}'.format(doc))
-    #     bad = read_text('textdocs/{}'.format(doc))
-    #     n = len(correct)
-    #     E = distance(correct, bad)
-    #     print '{0} character accuracy: {1:.2f}%'.format(doc, (n - E) / n)
-    # for doc in docs:
-    #     text = read_text('train_text/{}'.format(doc))
-    #     trie = make_trie(text)
-    #     with open('tries/{0}_ls.json'.format(doc.replace('.txt', '')), 'w') as f:
-    #         json.dump(trie, f)
-    #     trie = make_trie(text[::-1])
-    #     with open('tries/{0}_lp.json'.format(doc.replace('.txt', '')), 'w') as f:
-    #         json.dump(trie, f)
-    with open('tries/DOC100S1082_0_ls.json') as f:
-        trie = json.load(f)
+    docs = [f for f in os.listdir('textdocs/') if f.endswith('.txt')]
+    for doc in docs:
+        correct = read_text('train_text/{}'.format(doc))
+        bad = read_text('textdocs/{}'.format(doc))
+        n = len(correct)
+        E = distance(correct, bad)
+        print '{0} character accuracy: {1:.2f}%'.format(doc, (n - E) / n)
+    for doc in docs:
+        text = read_text('train_text/{}'.format(doc))
+        trie = make_trie(text)
+        with open('tries/{0}_f.json'.format(doc.replace('.txt', '')), 'w') as f:
+            json.dump(trie, f)
+        trie = make_trie(text[::-1])
+        with open('tries/{0}_b.json'.format(doc.replace('.txt', '')), 'w') as f:
+            json.dump(trie, f)
+    with open('tries/DOC100S1082_0_f.json') as f:
+        f_trie = json.load(f)
+    with open('tries/DOC100S1082_0_b.json') as f:
+        b_trie = json.load(f)
+    text = read_text('textdocs/DOC100S1082_0.txt')
+    windows = segment_text(text, f_trie, b_trie)

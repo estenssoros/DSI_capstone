5c7b618da1bf7bc2480713199ecbd9f07ff15cd7
estenssoros <seb.estenssoro@gmail.com>
2016-08-02 11:56:09 -0600

    updated for ec2 parsing

--- landman/wc_clerk.py
+++ landman/wc_clerk.py
@@ -9,7 +9,7 @@ from string import maketrans, punctuation
 import os
 import json
 import numpy as np
-from lsseg import text_to_tries, Segmenter
+from lsseg import text_to_tries, Segmenter, seg_to_HTML, format_line, text_to_HTML
 
 
 def get_text_df(fname):
@@ -33,6 +33,7 @@ def read_text(fname, doc=False):
     text = ' '.join(text.split())
     ind = text.find('oil')
     text = text[ind:]
+
     if doc:
         base = os.path.basename(fname)
         doc = os.path.splitext(base)[0]
@@ -40,6 +41,38 @@ def read_text(fname, doc=False):
         end = '---{0}---'.format(doc)
         return start + text + end
     return text
+
+
+def find_words(arg, vocab=None, maxword=None):  # , keywords=None):
+    doc_num, text = arg
+    if vocab is None:
+        d = 'traintext/'
+        docs = [d + f for f in os.listdir(d) if f.endswith('.txt')]
+        words = ' '.join([read_text(doc) for doc in docs])
+        vocab = list(set(words.split()))
+
+    if maxword is None:
+        maxword = max(len(x) for x in vocab)
+
+    word_arr, missing = [], []
+    while len(text) > 0:
+        start = 0
+        end = start + 1
+        options = []
+        for i in range(maxword):
+            test_word = text[start:end]
+            if test_word in vocab:
+                options.append(test_word)
+            end += 1
+
+        if options:
+            option = max(options, key=lambda x: len(x))
+            text = text[len(option):]
+            word_arr.append(option)
+        else:
+            text = text[1:]
+
+    return doc, ' '.join(word_arr)
 
 
 def find_legal_description(arg):
@@ -81,67 +114,47 @@ def multi_func(df, func, columns):
 
 def parse_legal_descr(arg):
     doc, text = arg
-
     # township
-    towns = re.findall('town\w+ [\d ]+|town\w+ [\d]+|[\d]+ north|[\d ]+ north', text)
+    towns = re.findall('town\w+ [\d ]+|town\w+ [\d]+', text)
     for t in towns:
         text = text.replace(t, '')
-    text = text.strip()
-    if text.startswith('north'):
-        text = text[5:].strip()
 
+    # range
     range_text = ''.join(text.split())
     ranges = re.findall('ran\w+ [\d ]+|ran\w+ [\d]+', range_text)
     for r in ranges:
         text = text.replace(r, '')
-    text = text.strip()
 
     # section
     secs = re.findall('sec\w+ [\d ]+|sec\w+ [\d]+', text)
-
     for s in secs:
         text = text.replace(s, '')
-    text = text.strip()
-
-    # range
-    text = ''.join(text.split())
-    ranges.extend(re.findall('\d+w', text))
 
     towns = [re.findall('\d+', ''.join(t.split())) for t in towns]
-    secs = [re.findall('\d+', ''.join(s.split())) for s in secs]
     ranges = [re.findall('\d+', ''.join(r.split())) for r in ranges]
+    secs = [re.findall('\d+', ''.join(s.split())) for s in secs]
 
-    return doc, towns, ranges, secs  # , quarters
+    return doc, towns, ranges, secs
 
 
 def apply_funcs(df):
+
+    print 'find legal description'
     new_df = multi_func(df[['doc', 'clean_text']], find_legal_description, ['doc', 'legal_text'])
-    print 'legal description'
     df = pd.merge(df, new_df, how='left', on='doc')
 
-    new_df = multi_func(df[['doc', 'clean_text']], find_years, ['doc', 'years'])
     print 'lease years'
+    new_df = multi_func(df[['doc', 'clean_text']], find_years, ['doc', 'years'])
     df = pd.merge(df, new_df, how='left', on='doc')
 
+    print 'parse legal description'
     new_df = multi_func(df[['doc', 'legal_text']], parse_legal_descr, ['doc', 'town', 'range', 'section'])
-    print 'legal description'
     df = pd.merge(df, new_df, how='left', on='doc')
+
     return df
 
 
-if __name__ == '__main__':
-    os.system('clear')
-    # df = get_text_df('data/text_data_sample.csv')
-    # df = multi_func(df, find_words, ['doc', 'clean_text'])
-    # df.to_pickle('data/corrected_text.pickle')
-    # df = apply_funcs(df)
-    # # # # df.to_pickle('data/all_data.pickle')
-    # df = pd.read_pickle('data/all_data.pickle')
-
-    fname = 'textdocs/DOC100S825_0.txt'
-    text = read_text(fname)
-    # text = ''.join(text.split())
-
+def segment_text(doc_str):
     d = 'textdocs/'
     docs = [d + f for f in os.listdir(d) if f.endswith('.txt')]
     text = ' '.join([read_text(d, doc=True) for d in docs])
@@ -150,15 +163,33 @@ if __name__ == '__main__':
     docs = [d + f for f in os.listdir(d) if f.endswith('.txt')]
     train_text = ' '.join([read_text(d) for d in docs])
     train_text = '---train---' + train_text + '---train---'
-    
-    char_window = 5
-    lower_case = True
-    peak_type = 'entropy'
-    threshold = 0.5
 
-    f_trie, b_trie = text_to_tries(text, lowercase=True)
+    char_window = 4
+    lower_case = True
+    peak_type = 'freedom'
+    threshold = 0.8
+
+    f_trie, b_trie = text_to_tries(train_text, lowercase=True)
     seg = Segmenter(f_trie, b_trie, window=char_window, lowercase=lower_case, peak=peak_type)
 
-    mer = seg.segment(text, threshold=threshold)
-    for i in range(10):
-        print '\a'
+    mer = ' '.join(seg.segment(text, threshold=threshold))
+    mer = ' '.join(mer.split())
+    indx = [m.start() for m in re.finditer(doc_str, mer)]
+    with open('results_{0}_{1}_{2}.txt'.format(char_window, peak_type[0].upper(), threshold), 'w') as f:
+        f.write(mer[indx[0] + len(doc_str):indx[1]])
+
+    print 'done!'
+if __name__ == '__main__':
+    os.system('clear')
+    df = get_text_df('data/text_data.csv')
+    df = multi_func(df, find_words, ['doc', 'clean_text'])
+    df.to_pickle('data/corrected_text.pickle')
+
+    df = pd.read_pickle('data/corrected_text.pickle')
+    df = apply_funcs(df)
+    df.to_pickle('data/all_data.pickle')
+
+    # fname = 'textdocs/DOC100S825_0.txt'
+    # text = read_text(fname)
+    # text = ''.join(text.split())
+    # word_arr = find_words(text)
